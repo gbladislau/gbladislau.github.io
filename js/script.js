eb443f350dbef7230e18a5b24553d1c44e0d659c
Gabriel Braga Ladislau <69354723+gbladislau@users.noreply.github.com>
2025-05-15 21:10:26 +0000

    Update Page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 if (document.getElementById('my-work-link')) {
   document.getElementById('my-work-link').addEventListener('click', () => {
-    document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
+    window.open("https://github.com/gbladislau", "_blank");    
   })
 }
